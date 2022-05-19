0ad97209d4f23e0be199dfb66abb9a514962e609
zograf <lazzarmilanovic@gmail.com>
2022-05-19 12:20:35 +0200

    prewitt serial

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,42 @@
 
 #define __ARG_NUM__				6
 #define FILTER_SIZE				3
-#define THRESHOLD				128
+#define THRESHOLD				30
 
 using namespace std;
 
 // Prewitt operators
-int filterHor[FILTER_SIZE * FILTER_SIZE] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
-int filterVer[FILTER_SIZE * FILTER_SIZE] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+const int horizontal_filter[FILTER_SIZE * FILTER_SIZE] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
+const int vertical_filter[FILTER_SIZE * FILTER_SIZE] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
+
+void multiply(int* in_matrix, int* out_matrix, int x, int y, int width) { 
+    int sum_h = 0, sum_v = 0;
+    for (int i = 0; i < FILTER_SIZE; i++) {
+        for (int j = 0; j < FILTER_SIZE; j++) {
+            sum_h += horizontal_filter[i * FILTER_SIZE + j] * in_matrix[(x + i) * width + (y + j)];
+            sum_v += vertical_filter[i * FILTER_SIZE + j] * in_matrix[(x + i) * width + (y + j)];
+        }
+    }
+    // int total = (int) sqrt((sum_h * sum_h + sum_v * sum_v));
+    int total = abs(sum_h) + abs(sum_v);
+    out_matrix[x * width + y] = (total > THRESHOLD) ? 255 : 0;
+}
 
 /**
 * @brief Serial version of edge detection algorithm implementation using Prewitt operator
+*
 * @param inBuffer buffer of input image
 * @param outBuffer buffer of output image
 * @param width image width
 * @param height image height
 */
-void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)  //TODO obrisati
-{
+void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height) {
+    for (int i = 1; i < height - 1; i++) {
+        for (int j = 1; j < width - 1; j++) {
+            multiply(inBuffer, outBuffer, i, j, width);
+        }
+    }
 }
-
 
 /**
 * @brief Parallel version of edge detection algorithm implementation using Prewitt operator
@@ -34,19 +51,18 @@ void filter_serial_prewitt(int *inBuffer, int *outBuffer, int width, int height)
 * @param width image width
 * @param height image height
 */
-void filter_parallel_prewitt(int *inBuffer, int *outBuffer, int width, int height)
-{
+void filter_parallel_prewitt(int *inBuffer, int *outBuffer, int width, int height) {
 }
 
 /**
 * @brief Serial version of edge detection algorithm
+*
 * @param inBuffer buffer of input image
 * @param outBuffer buffer of output image
 * @param width image width
 * @param height image height
 */
-void filter_serial_edge_detection(int *inBuffer, int *outBuffer, int width, int height)	//TODO obrisati
-{
+void filter_serial_edge_detection(int *inBuffer, int *outBuffer, int width, int height) {
 }
 
 /**
@@ -57,8 +73,7 @@ void filter_serial_edge_detection(int *inBuffer, int *outBuffer, int width, int 
 * @param width image width
 * @param height image height
 */
-void filter_parallel_edge_detection(int *inBuffer, int *outBuffer, int width, int height)
-{
+void filter_parallel_edge_detection(int *inBuffer, int *outBuffer, int width, int height) {
 }
 
 /**
@@ -73,14 +88,11 @@ void filter_parallel_edge_detection(int *inBuffer, int *outBuffer, int width, in
 */
 
 
-void run_test_nr(int testNr, BitmapRawConverter* ioFile, char* outFileName, int* outBuffer, unsigned int width, unsigned int height)
-{
+void run_test_nr(int testNr, BitmapRawConverter* ioFile, 
+        char* outFileName, int* outBuffer, unsigned int width, unsigned int height) {
 
-	// TODO: start measure
-	
-
-	switch (testNr)
-	{
+    // TODO: measure time
+	switch (testNr) {
 		case 1:
 			cout << "Running serial version of edge detection using Prewitt operator" << endl;
 			filter_serial_prewitt(ioFile->getBuffer(), outBuffer, width, height);
@@ -107,8 +119,8 @@ void run_test_nr(int testNr, BitmapRawConverter* ioFile, char* outFileName, int*
 	ioFile->pixelsToBitmap(outFileName);
 }
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
+    // Output files
     char* output[4];
     output[0] = (char*) "../../out/serialPrewitt.bmp";
     output[1] = (char*) "../../out/parallelPrewitt.bmp";
@@ -141,6 +153,8 @@ int main(int argc, char * argv[])
 	memset(outBufferSerialEdge, 0x0, width * height * sizeof(int));
 	memset(outBufferParallelEdge, 0x0, width * height * sizeof(int));
 
+	inputFile.setBuffer(outBufferSerialPrewitt);
+	inputFile.pixelsToBitmap(output[0]);
 	// serial version Prewitt
 	run_test_nr(1, &outputFileSerialPrewitt, output[0], outBufferSerialPrewitt, width, height);
 
@@ -154,29 +168,23 @@ int main(int argc, char * argv[])
 	run_test_nr(4, &outputFileParallelEdge, output[3], outBufferParallelEdge, width, height);
 
 	// verification
+    /*
 	cout << "Verification: ";
 	test = memcmp(outBufferSerialPrewitt, outBufferParallelPrewitt, width * height * sizeof(int));
 
 	if(test != 0)
-	{
 		cout << "Prewitt FAIL!" << endl;
-	}
-	else
-	{
+    else
 		cout << "Prewitt PASS." << endl;
-	}
 
 	test = memcmp(outBufferSerialEdge, outBufferParallelEdge, width * height * sizeof(int));
 
 	if(test != 0)
-	{
 		cout << "Edge detection FAIL!" << endl;
-	}
 	else
-	{
 		cout << "Edge detection PASS." << endl;
-	}
 
+    */
 	// clean up
 	delete[] outBufferSerialPrewitt;
 	delete[] outBufferParallelPrewitt;
